ab015501efc940fcafc261bcd9cf75b77139f92b
ohmyguigs <ohmyguigs@gmail.com>
2019-09-14 17:26:23 -0300

    CORS

--- react/index.tsx
+++ react/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react'
 
-const App: React.FC<any> = () => {
-  return <span>oi</span>
-}
+import { PageBlock, Alert } from 'vtex.styleguide'
 
-export default App
+export default class App extends React.Component {
+  public state: {
+    wifiStatus: boolean
+    slackStatus: boolean
+  }
+  public constructor(props: any) {
+    super(props)
+    this.state = {
+      wifiStatus: true,
+      slackStatus: true,
+    }
+  }
+
+  public componentDidMount() {
+    window.addEventListener('online', () => this.setState({ wifiStatus: true }))
+    window.addEventListener('offline', () =>
+      this.setState({ wifiStatus: false })
+    )
+    const loop = () =>
+      setTimeout(async () => {
+        const res = await fetch('https://status.slack.com/api/v2.0.0/current')
+        console.log('ews ', res)
+        loop()
+      }, 1000)
+    loop()
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener('online', () => {})
+    window.removeEventListener('offline', () => {})
+  }
+
+  public render() {
+    const { wifiStatus } = this.state
+    return (
+      <div
+        className="flex justify-center pb7 bg-muted-5 min-vh-100"
+        style={{ backgroundColor: '#333' }}>
+        <div className="w-100 pa5">
+          <PageBlock variation="half">
+            <div>
+              <Alert type={wifiStatus ? 'success' : 'error'}>{`wifi is ${
+                wifiStatus ? 'online' : 'offline'
+              }`}</Alert>
+            </div>
+            <div>wifi data</div>
+          </PageBlock>
+        </div>
+      </div>
+    )
+  }
+}
