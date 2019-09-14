d5fb65fbdb93f571abab3e1a52a4898c8c3f1b20
ohmyguigs <ohmyguigs@gmail.com>
2019-09-14 19:06:02 -0300

    Mock a speedometer

--- react/index.tsx
+++ react/index.tsx
@@ -1,31 +1,47 @@
+/* eslint-disable no-console */
 import React from 'react'
-
-import { PageBlock, Alert } from 'vtex.styleguide'
+import ReactSpeedometer from 'react-d3-speedometer'
+import { Tag, PageBlock, Alert } from 'vtex.styleguide'
 
 export default class App extends React.Component {
   public state: {
     wifiStatus: boolean
     slackStatus: boolean
+    wifiSpeed: number
   }
   public constructor(props: any) {
     super(props)
     this.state = {
       wifiStatus: true,
       slackStatus: true,
+      wifiSpeed: 420,
     }
   }
 
-  public componentDidMount() {
+  public async componentDidMount() {
     window.addEventListener('online', () => this.setState({ wifiStatus: true }))
     window.addEventListener('offline', () =>
       this.setState({ wifiStatus: false })
     )
-    const loop = () =>
-      setTimeout(async () => {
-        const res = await fetch('https://status.slack.com/api/v2.0.0/current')
-        console.log('ews ', res)
+    const getRandomInt = (min: number, max: number) => {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min)) + min
+    }
+    const loop: any = () =>
+      setTimeout(() => {
+        const { wifiSpeed } = this.state
+        this.setState({
+          wifiSpeed:
+            wifiSpeed > 440
+              ? wifiSpeed - getRandomInt(5, 10)
+              : wifiSpeed < 400
+              ? wifiSpeed + getRandomInt(5, 10)
+              : wifiSpeed - getRandomInt(5, 10),
+        })
         loop()
-      }, 1000)
+      }, getRandomInt(420, 840))
+
     loop()
   }
 
@@ -35,19 +51,40 @@ export default class App extends React.Component {
   }
 
   public render() {
-    const { wifiStatus } = this.state
+    const { wifiStatus, wifiSpeed } = this.state
     return (
       <div
         className="flex justify-center pb7 bg-muted-5 min-vh-100"
         style={{ backgroundColor: '#333' }}>
         <div className="w-100 pa5">
-          <PageBlock variation="half">
+          <PageBlock variation="aside">
             <div>
               <Alert type={wifiStatus ? 'success' : 'error'}>{`wifi is ${
                 wifiStatus ? 'online' : 'offline'
               }`}</Alert>
             </div>
-            <div>wifi data</div>
+            <div className="flex flex-column">
+              <div className="center" style={{ width: 300, height: 170 }}>
+                <ReactSpeedometer
+                  maxValue={500}
+                  value={wifiSpeed}
+                  needleColor="black"
+                  startColor="red"
+                  segments={10}
+                  endColor="green"
+                />
+              </div>
+              <span className="mt4 tc">
+                WiFi Speed
+                <span className="ml4">
+                  <Tag
+                    type={wifiSpeed > 400 ? 'success' : 'warning'}
+                    variation="low">
+                    {`ping ${Math.floor(wifiSpeed / 10)}`}
+                  </Tag>
+                </span>
+              </span>
+            </div>
           </PageBlock>
         </div>
       </div>
