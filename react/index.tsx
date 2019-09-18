b18803df77f8b6ffdd0bdeb53b9383f8cfa8ae82
ohmyguigs <ohmyguigs@gmail.com>
2019-09-18 20:41:03 -0300

    Mock some more status cards and states

--- react/index.tsx
+++ react/index.tsx
@@ -9,12 +9,20 @@ export default class App extends React.Component {
   public state: {
     wifiStatus: boolean
     slackStatus: boolean
+    githubStatus: boolean
+    gmailStatus: boolean
+    googleOAuthStatus: boolean
+    cloudflareStatus: boolean
   }
   public constructor(props: any) {
     super(props)
     this.state = {
       wifiStatus: true,
       slackStatus: true,
+      githubStatus: true,
+      gmailStatus: true,
+      googleOAuthStatus: true,
+      cloudflareStatus: true,
     }
   }
 
@@ -31,11 +39,18 @@ export default class App extends React.Component {
   }
 
   public render() {
-    const { wifiStatus } = this.state
+    const {
+      wifiStatus,
+      slackStatus,
+      githubStatus,
+      gmailStatus,
+      googleOAuthStatus,
+      cloudflareStatus,
+    } = this.state
     return (
       <div
         className="flex justify-center pb7 bg-muted-5 min-vh-100"
-        style={{ backgroundColor: '#333' }}>
+        style={{ backgroundColor: '#666' }}>
         <div className="w-100 ph10 pv5">
           <div className="flex flex-row items-center">
             <Logo />
@@ -43,9 +58,47 @@ export default class App extends React.Component {
           </div>
           <PageBlock variation="aside">
             <div>
-              <Alert type={wifiStatus ? 'success' : 'error'}>{`wifi is ${
-                wifiStatus ? 'online' : 'offline'
-              }`}</Alert>
+              <div className="mb3">
+                <Alert type={wifiStatus ? 'success' : 'error'}>{`wifi is ${
+                  wifiStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
+              <div className="mb3">
+                <Alert type={slackStatus ? 'success' : 'error'}>{`Slack is ${
+                  slackStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
+              <div className="mb3">
+                <Alert type={githubStatus ? 'success' : 'error'}>{`Github is ${
+                  githubStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
+              <div className="mb3">
+                <Alert type={githubStatus ? 'success' : 'error'}>{`Github is ${
+                  githubStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
+              <div className="mb3">
+                <Alert type={gmailStatus ? 'success' : 'error'}>{`Gmail is ${
+                  gmailStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
+              <div className="mb3">
+                <Alert
+                  type={
+                    googleOAuthStatus ? 'success' : 'error'
+                  }>{`Google OAuth is ${
+                  googleOAuthStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
+              <div className="mb3">
+                <Alert
+                  type={
+                    cloudflareStatus ? 'success' : 'error'
+                  }>{`Cloud Flare is ${
+                  cloudflareStatus ? 'online' : 'offline'
+                }`}</Alert>
+              </div>
             </div>
             <Speedometer />
           </PageBlock>
